bb2da7fd39ae6fc03f544e79aa79b29e92976dd3
Anton Kranarenko <a.kramarenko@evopay.com.ua>
2024-04-25 00:22:44 +0300

    fix irregular verbs table

--- src/pages/IrregularVerbsTable/IrregularVerbsTable.tsx
+++ src/pages/IrregularVerbsTable/IrregularVerbsTable.tsx
@@ -7,59 +7,76 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Paper, styled,
+  Paper, styled, Grid,
 } from '@mui/material';
 import {irregularVerbsData} from "./constants";
 
-
-const StyledTableContainer = styled(TableContainer)`
-  width: 100%;
-  overflow-x: auto;
-`;
-
-const StyledTable = styled(Table)`
-  width: 100%;
-`;
-
-const StyledTableCell = styled(TableCell)`
-  border: 1px solid #dddddd;
-  text-align: center;
-  padding: 8px;
-`;
-
-const StyledTableHeaderCell = styled(TableCell)`
-  border: 1px solid #dddddd;
-  text-Align: center;
-  padding: 8px;
-  font-weight: bold;
-`;
-
 export const IrregularVerbsTable = () => {
-  return (
-    <TableContainer component={Paper} sx={{ maxWidth: 650, boxShadow: 'none' }}>
-      <Typography mb={3} mt={3} variant='h2' gutterBottom>
-        Неправильні дієслова
-      </Typography>
-      <StyledTableContainer>
-        <StyledTable>
-          <TableHead>
-            <TableRow>
-              <StyledTableHeaderCell>INFINITIVE</StyledTableHeaderCell>
-              <StyledTableHeaderCell>PAST SIMPLE</StyledTableHeaderCell>
-              <StyledTableHeaderCell>PAST PARTICIPLE</StyledTableHeaderCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {irregularVerbsData.map((row, index) => (
-              <TableRow key={index}>
-                <StyledTableCell>{row.infinitive}</StyledTableCell>
-                <StyledTableCell>{row.pastSimple}</StyledTableCell>
-                <StyledTableCell>{row.pastParticiple}</StyledTableCell>
+
+
+  return  <Grid container>
+    <Typography mb={3} mt={3} variant='h2' gutterBottom>
+      Неправильні дієслова
+    </Typography>
+
+
+    <Grid container spacing={3} >
+      <Grid item xs={4}>
+        <TableContainer component={Paper} sx={{ border: '1px solid rgba(231, 238, 243, 1)', p: 2}}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ border: 'none'}}>INFINITIVE</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </StyledTable>
-      </StyledTableContainer>
-    </TableContainer>
-  );
+            </TableHead>
+            <TableBody>
+              {irregularVerbsData.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell sx={{ border: 'none'}}>{row.infinitive}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+
+      <Grid item xs={4}>
+        <TableContainer component={Paper} sx={{ border: '1px solid rgba(231, 238, 243, 1)', p: 2}}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ border: 'none'}}>PAST SIMPLE</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {irregularVerbsData.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell sx={{ border: 'none'}}>{row.pastSimple}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+
+      <Grid item xs={4}>
+        <TableContainer component={Paper} sx={{ border: '1px solid rgba(231, 238, 243, 1)', p: 2}}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ border: 'none'}}>PAST PARTICIPLE</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {irregularVerbsData.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell sx={{ border: 'none'}}>{row.pastParticiple}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
+  </Grid>
 };
